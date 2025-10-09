399cc9ed7ccf50b4f3e3b630ef0a2ef603595141
matteokosina <matteokosina@icloud.com>
2025-10-09 17:31:17 +0200

    chore: add a pipeline step to automatically generate OpenAPI Docs via Swagger

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 // @description	API Central Entrypoint for the Enclave Platform
 // @license.name	GNU General Public License v3.0
 // @license.url	https://www.gnu.org/licenses/gpl-3.0.html
-// @host			localhost:8080 (default)
+// @host			localhost:8080
 func main() {
 	// load config and create server
 	shareddeps.Init(config.Cfg, "api-server", "v0.0.0", nil, nil)

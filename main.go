27977bee482dd28ed62eb4ff56e0db40d0f7fb81
Marvin Fuchs <info@marvin-fuchs.de>
2025-10-23 14:00:01 +0200

    Add rbac default and adjust tests

--- main.go
+++ main.go
@@ -6,15 +6,11 @@ import (
 	"api-server/orm"
 
 	"github.com/EnclaveRunner/shareddeps"
+	"github.com/EnclaveRunner/shareddeps/auth"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
 
-// @title			Enclave API Server
-// @version			v0.0.0
-// @description	API Central Entrypoint for the Enclave Platform
-// @license.name	GNU General Public License v3.0
-// @license.url	https://www.gnu.org/licenses/gpl-3.0.html
-// @host			localhost:8080
 func main() {
 	// Set configuration defaults
 	//nolint:mnd // Default port for PostgreSQL database
@@ -41,9 +37,95 @@ func main() {
 	// Initialize admin user after auth system is ready
 	orm.InitAdminUser()
 
+	// Migrate RBAC policies, resource groups and roles
+	MigrateRBAC()
+
 	server := api.NewServer()
 	handler := api.NewStrictHandler(server, nil)
 	api.RegisterHandlers(shareddeps.Server, handler)
 
 	shareddeps.Start()
+}
+
+// Init needed and default RBAC policies, resource groups and roles
+func MigrateRBAC() {
+	resourceGroups := []string{
+		"self_INTERNAL",
+		"user_management",
+		"rbac_management",
+	}
+
+	userGroups := []string{
+		"read_only",
+		"user_management",
+		"rbac_management",
+	}
+
+	// Define resource to group mappings
+	type ResourceMapping struct {
+		Resource string
+		Group    string
+	}
+	resourceMappings := []ResourceMapping{
+		{"/users/me", "self_INTERNAL"},
+		{"/users/user", "user_management"},
+		{"/users/list", "user_management"},
+		{"/rbac/list-roles", "rbac_management"},
+		{"/rbac/role", "rbac_management"},
+		{"/rbac/user", "rbac_management"},
+		{"/rbac/list-resource-groups", "rbac_management"},
+		{"/rbac/resource-group", "rbac_management"},
+		{"/rbac/endpoint", "rbac_management"},
+		{"/rbac/policy", "rbac_management"},
+	}
+
+	// Define policies
+	type Policy struct {
+		UserGroup     string
+		ResourceGroup string
+		Method        string
+	}
+	policies := []Policy{
+		{"*", "self_INTERNAL", "*"},
+		{"read_only", "*", "GET"},
+		{"read_only", "*", "HEAD"},
+		{"user_management", "user_management", "*"},
+		{"rbac_management", "rbac_management", "*"},
+	}
+
+	// Create resource groups
+	for _, group := range resourceGroups {
+		err := auth.CreateResourceGroup(group)
+		if err != nil {
+			log.Fatal().Err(err).Msgf("Failed to create resource group: %s", group)
+		}
+	}
+
+	// Create user groups
+	for _, group := range userGroups {
+		err := auth.CreateUserGroup(group)
+		if err != nil {
+			log.Fatal().Err(err).Msgf("Failed to create user group: %s", group)
+		}
+	}
+
+	// Add resources to groups
+	for _, mapping := range resourceMappings {
+		err := auth.AddResourceToGroup(mapping.Resource, mapping.Group)
+		if err != nil {
+			log.Fatal().
+				Err(err).
+				Msgf("Failed to add resource %s to group %s", mapping.Resource, mapping.Group)
+		}
+	}
+
+	// Add policies
+	for _, policy := range policies {
+		err := auth.AddPolicy(policy.UserGroup, policy.ResourceGroup, policy.Method)
+		if err != nil {
+			log.Fatal().
+				Err(err).
+				Msgf("Failed to add policy: %s -> %s [%s]", policy.UserGroup, policy.ResourceGroup, policy.Method)
+		}
+	}
 }
